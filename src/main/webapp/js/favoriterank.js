5d6f547812fae0859c9a0dd9e6e60ddd7a55276b
RamEMT <2048671259@qq.com>
2020-10-22 10:06:54 +0800

    前端数据展示、分页 未完成

---
+++ src/main/webapp/js/favoriterank.js
@@ -0,0 +1,69 @@
+const BASE_URL = "http://localhost:8080/api/topAll";
+
+const BASE_CONDITION_URL = "http://localhost:8080/api/topCondition";
+
+$(function () {
+    let index = 1;
+    
+    let params = {
+        page: index
+    }
+    $.get(BASE_URL,params,function (result) {
+        if (result.code === 200){
+            //数据展示
+            showTopFavorite(result.data.list);
+        }
+    })
+
+    // <li>
+    //     <span class="num one">1</span>
+    //     <a href="route_detail.html"><img src="images/jiangxuan_4.jpg" alt=""></a>
+    //     <h4><a href="route_detail.html">【尾单特卖】全国-曼谷6-7天自由行 泰国出境旅游 特价往6-7天自由行 泰国出境旅游 特价往..</a></h4>
+    //     <p>
+    //         <b class="price">&yen;<span>899</span>起</b>
+    //         <span class="shouchang">已收藏450次</span>
+    //     </p>
+    // </li>
+    function showTopFavorite(topCollectAll) {
+        let index = 1;
+        for (const topSingle of topCollectAll) {
+            $("#topCollect").append(
+                $("<li>").append(
+                    $("<span>").addClass("num one").text(index++)
+                ).append(
+                    $("<a>").attr("href","route_detail.html?id="+topSingle.rid).append(
+                        $("<img>").attr("src",topSingle.rimage)
+                    )
+                ).append(
+                    $("<h4>").append(
+                        $("<a>").attr("href","route_detail.html?id="+topSingle.rid).text(topSingle.rname)
+                    )
+                ).append(
+                    $("<p>").append(
+                        $('<b class="price">').text("￥"+topSingle.price).append(
+                            $("<span>").text("起")
+                        )
+                    ).append(
+                        $('<span class="shouchang">').text("已收藏"+topSingle.count+"次")
+                    )
+                )
+            )
+        }
+    }
+
+
+    
+    $("#content").submit(function () {
+        //获取输入框的内容
+        let params = {
+            title: $("#title").val(),
+            minPrice: $("#min").val(),
+            maxPrice: $("#max").val()
+        }
+        $.get(BASE_CONDITION_URL,params,function (result) {
+            if (result.code === 200){
+                showTopFavorite(result.data.list);
+            }
+        })
+    })
+})
